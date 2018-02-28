cf3e12f9b6ce045a6b9c3daea76402fbd09a85e4
Spiritgyx <gyxepm@gmail.com>
2018-02-28 23:15:59 +0300

    banking

--- lab1/bank.py
+++ lab1/bank.py
@@ -11,7 +11,7 @@ def main():
     for i in range(len(converter.v)):
         a = converter.convert(money, i)
         b = converter.convert(result, i)
-        print("Параметры счета {0}:\n".format(a[0]), "Сумма ({0}): {1}".format(a[0], a[1]), "\n", "Ставка: ", rate, "\n",
+        print("Параметры счета ({0}):\n".format(a[0]), "Сумма ({0}): {1}".format(a[0], a[1]), "\n", "Ставка: ", rate, "\n",
               "Период: ", period, "\n", "Сумма на счете в конце периода ({0}): {1}".format(b[0], b[1]))
 
 
